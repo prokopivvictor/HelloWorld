a90df072b30facb86a33beed69cfad4e7920bbfc
prokopivvictor <p.victor2606@gmail.com>
2014-06-05 12:25:55 +0600

    plz, let it work

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 05.06.2014.
+ */
+public class Runner{
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+
+    }
+}
